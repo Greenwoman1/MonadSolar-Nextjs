9a4222224ab87b9e9b3351fdb1044aa7189cf656
Erna Berbic <ernaeniberbic@gmail.com>
2024-03-14 10:52:35 +0100

    radim navbar

--- moneshop/src/app/layout.js
+++ moneshop/src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/navbar/page";
+import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
+        <Navbar />
 
         {children}
 
---
+++ moneshop/src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "./Navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="layout">
+        <div className='middle'>
+          <div className={styles.navbar_container}>
+            
+          </div>
+        </div>
+      </div>  
+    </>
+  )
+}
+
+export default Navbar
