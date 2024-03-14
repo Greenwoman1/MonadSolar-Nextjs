3970677d7b6f70dcbcf2e7caf4daf19a75cf83a3
sara <sara12travnik@gmail.com>
2024-03-14 11:25:29 +0100

    header

--- monadsolar/src/app/page.js
+++ monadsolar/src/app/page.js
@@ -1,13 +1,12 @@
-
-import ServiceOffer from "../components/service-offer/service-offer";
-import styles from "./home.module.css"
+import Header from "@/components/header/page";
+import styles from "./home.module.css";
+import "./globals.css"
 const Home = () => {
   return (
     <div className="layout">
-      <div className="middle">
-        <div className={styles.home}>
-          <ServiceOffer />
-         
+      <div className="firstRow-all-columns">
+        <div className={styles.container}>
+          <Header src={"./header-cover.jpg"} title={""} />
         </div>
       </div>
     </div>
--- monadsolar/src/components/header/page.jsx
+++ monadsolar/src/components/header/page.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
 
-const Header = () => {
+"use client"
+import styles from "./header.module.css";
+
+const Header = ({ src, title }) => {
+
+
+
+  const bannerImageStyle = () => {
+    if (title === "Products") {
+      return {
+        backgroundImage: `linear-gradient(
+            rgba(0, 0, 0, 0.6),
+            rgba(0, 0, 0, 0.6)
+          ),
+          url(${src})`,
+      };
+    } else if (title === "About us") {
+      return {
+        backgroundImage: `linear-gradient(
+          rgba(0, 0, 0, 0.6),
+          rgba(0, 0, 0, 0.6)
+          ),
+          url(${src})`,
+      };
+    } else if (title === "Contact Us") {
+      return {
+        backgroundImage: `linear-gradient(
+          rgba(0, 0, 0, 0.6),
+          rgba(0, 0, 0, 0.6)
+          ),
+          url(${src})`,
+      };
+    } else {
+      return {
+        backgroundImage: `linear-gradient(
+            rgba(0, 162, 255, 0.5),
+            rgba(255, 255, 255, 0.6)
+          ),
+          url(${src})`,
+      };
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className={styles.header}>
+      <div className={styles.banner_image} style={bannerImageStyle()}>
+        <div className={styles.banner_text}>
+          {title === "" ? (
+            <>
+              <h1>
+                <span style={{ color: "yellow" }}>M</span>onad
+                <span style={{ color: "yellow" }}>S</span>olar
+              </h1>
 
-export default Header
+              <p>Powering the Future with Solar Energy</p>
+            </>
+          ) : (
+            <>
+              <h1>{title}</h1>
+            </>
+          )}
+        </div>
+        <a
+          href={`#${title === "Contact Us" ? "contact_sec" : title === "Products" ? "products_sec" : title === "About us" ? "about_sec" : title === "" ? "sec1" : ""}`}
+          className={styles.banner_button}
+        >
+          <i className="fa-solid fa-chevron-down"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
