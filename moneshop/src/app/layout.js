140d3ec796fc315d90b5dc976ab358d319b9b696
Erna Berbic <ernaeniberbic@gmail.com>
2024-03-14 10:46:49 +0100

    footer gotov

--- moneshop/src/app/layout.js
+++ moneshop/src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar/page";
-import Footer from "@/components/footer/page";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
