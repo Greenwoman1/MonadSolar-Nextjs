c58377e6e7bdbeee35ea0d222ee4ccb4f2d00eaa
Edin Hasagic <edinhassagic@gmail.com>
2024-03-15 09:20:01 +0100

    update

--- monadsolar/src/app/layout.js
+++ monadsolar/src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
-import { Metadata } from "next";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
