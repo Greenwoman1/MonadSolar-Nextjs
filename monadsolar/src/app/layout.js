07a836a97ca9e08b3ebf72f89b07db6a53c064ab
sara <sara12travnik@gmail.com>
2024-03-15 14:20:39 +0100

    dev

--- monadsolar/src/app/layout.js
+++ monadsolar/src/app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 
 import Footer from "@/components/footer/Footer";
-import { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -18,7 +17,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+
+      <body className={inter.className}  suppressHydrationWarning={true}>
         <Navbar />
 
         {children}
