f31b57b589ef88b8ea5d3d1520ff5608b723b0d5
sara <sara12travnik@gmail.com>
2024-03-18 11:56:32 +0100

    devvvvvvvvvv paginatioooon

--- monadsolar/src/app/blog/page.jsx
+++ monadsolar/src/app/blog/page.jsx
@@ -2,15 +2,35 @@ import Blogs from "./Blogs";
 import Pagination from "./Pagination";
 import { Suspense } from "react";
 const Blog = async ({ searchParams }) => {
+  const page = parseInt(searchParams?.page || 1);
+
+
+  const data = await fetch(`http://localhost:3001/blogs?page=${page}`, {
+    cache: "no-store",
+  })
+    .then((response) => response.json())
+    .then(({ data }) => data);
+
+
+
+
+    const  pagination = await fetch(
+      `http://localhost:3001/blogs?page=${page}`, {
+        cache: 'no-store'
+      }
+    )
+      .then((response) => response.json())
+      .then(({  paggination }) => ( paggination ));
+   
   return (
     <>
       <div className="layout">
         <div className="middle">
           <Suspense fallback={<div>Loading</div>}>
-            <Blogs />
+            <Blogs data = {data} />
           </Suspense>
           <Suspense fallback={<div>Loading</div>}>
-            <Pagination />
+            <Pagination page = {page} pagination = {pagination}/>
           </Suspense>
         </div>
       </div>
