ebd46b9157a074d847a1b594b6b469779a7d381f
Erna Berbic <ernaeniberbic@gmail.com>
2024-03-19 11:59:13 +0100

    kghzsrhgnbcnbvjzfzutfiuohz

--- monadsolar/src/app/blog/Blogs.jsx
+++ monadsolar/src/app/blog/Blogs.jsx
@@ -2,28 +2,27 @@ import { Suspense } from "react";
 import styles from "./Blog.module.css";
 import BlogCard from "@/components/BlogCard/BlogCard";
 
-const Blogs = async ({data}) => {
-
+const Blogs = async ({ data }) => {
   return (
-    <>
-      <div className={styles.blog_cards_wrapper}>
-        <div className={styles.blog_cards_grid}>
-          {data.map((card, index) => (
-            <Suspense key={index} fallback={<div className={styles.loading_block}></div>}>
-              <BlogCard
-                key={index}
-                id={card.id}
-                image={card.image}
-                title={card.title}
-                description={card.description}
-                date={card.date}
-              />
-            </Suspense>
-
-          ))}
-        </div>
+    <div className={styles.blog_cards_wrapper}>
+      <div className={styles.blog_cards_grid}>
+        {data.map((card, index) => (
+          <Suspense
+            key={index}
+            fallback={<div className={styles.loading_block}></div>}
+          >
+            <BlogCard
+              key={index}
+              id={card.id}
+              image={card.image}
+              title={card.title}
+              description={card.description}
+              date={card.date}
+            />
+          </Suspense>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
